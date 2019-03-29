d33104610f1b383702c76ac5b4505c8cdbbf049d
oskarijtmetropolia <oskarijt@metropolia.fi>
2019-03-29 11:10:41 +0200

    vue & express

---
+++ README.md
@@ -0,0 +1,4 @@
+Ports
+
+Backend     3000
+Frontend    8080
---
+++ server/index.js
@@ -0,0 +1,22 @@
+'use strict'
+const bodyParser = require('body-parser');
+const express = require('express');
+const cors = require('cors');
+
+var app = module.exports = express();
+
+require('dotenv').config();
+
+// Middleware
+app.use(bodyParser.urlencoded({extended:false}));   //handle body requests
+app.use(bodyParser.json());                         //makes JSON work
+app.use(cors());                                    //cross origin requests allowed
+app.use(express.static(__dirname + '../client'));
+
+const posts = require('./routes/api/posts');
+
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));

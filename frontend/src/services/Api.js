062ce711742bbf7ca9aac6cf8d4ceac97afbccec
oskarijtmetropolia <oskarijt@metropolia.fi>
2019-04-18 10:25:31 +0300

    url change

--- frontend/src/services/Api.js
+++ frontend/src/services/Api.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 
 export default () => {
     return axios.create({
-        baseURL: 'https://localhost:3000/api/auth'
+        //baseURL: 'https://localhost:3000/api/auth'
+        baseURL: 'https://env-1271826.jelastic.metropolia.fi:3000/api/auth'
     })
 }

ae633d344c7ea630be44e806ab112b014cf20ae2
oskarijt <oskari.j.tamminen@gmail.com>
2019-04-23 15:44:58 +0300

    url change fix

--- frontend/src/services/Api.js
+++ frontend/src/services/Api.js
@@ -3,6 +3,6 @@ const axios = require('axios');
 export default () => {
     return axios.create({
         //baseURL: 'https://localhost:3000/api/auth'
-        baseURL: 'https://env-1271826.jelastic.metropolia.fi:3000/api/auth'
+        baseURL: 'https://env-1271826.jelastic.metropolia.fi/api/auth'
     })
 }

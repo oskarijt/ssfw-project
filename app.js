ae221950f1713d0ef4ebc33d0157df3162032939
oskarijt <oskari.j.tamminen@gmail.com>
2019-04-29 13:43:37 +0300

    reviewer 2.0 beginning

---
+++ app.js
@@ -0,0 +1,43 @@
+'use strict'
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+
+// Conenct to DB
+mongoose.connect(`mongodb://${process.env.DB_HOST}:27017/week1`, 
+    { 
+        useNewUrlParser: true 
+    }
+    ).then(() => {
+        console.log('Connected successfully to db.');
+    }, err => {
+        console.log('Connection to db failed: ' + err);
+});
+
+
+// Middleware
+app.use(bodyParser.urlencoded({extended:true}));   //handle body requests
+app.use(bodyParser.json());                         //makes JSON work
+app.use(cors());                                    //cross origin requests allowed
+app.use(express.static('dist'));
+
+app.use((req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+});
+  
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+        message: error.message
+        }
+    });
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,15 @@
+const https = require('https');
+const app = require('./app');
+const fs = require('fs');
+
+const sslkey = fs.readFileSync('ssl-key.pem');
+const sslcert = fs.readFileSync('ssl-cert.pem')
+
+const options = {
+      key: sslkey,
+      cert: sslcert
+};
+
+const port = process.env.PORT || 3000;
+
+https.createServer(options, app).listen(port, () => console.log(`Server started on port ${port}`));
